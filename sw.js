bf57fbf45c7e928f1a152c68b6fdf9580c1fcf2b
y2kjoh23-ux <y2kjoh23@gmail.com>
2026-01-03 16:34:11 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+
+const CACHE_NAME = 'btc-compass-v9.9';
+const urlsToCache = ['./', './index.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
